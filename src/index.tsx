eacf0d8e6a8925ee9b04301cb1ca7c896e1aaec5
OliviaLB <olivia.l.bates@gmail.com>
2023-06-03 19:10:54 +0100

    Add scroll

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './hooks/scroll-top';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<BrowserRouter>
+		<ScrollToTop />
 		<App />
 	</BrowserRouter>
 );
