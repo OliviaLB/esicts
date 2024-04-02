800b773f73998fa4968cd7688f2fa22277dce6f2
OliviaLB <olivia.l.bates@gmail.com>
2024-04-02 10:38:23 +0100

    Fix footer

--- src/Components/Layout/Footer/Footer.tsx
+++ src/Components/Layout/Footer/Footer.tsx
@@ -1,94 +1,211 @@
-import classes from "./Footer.module.css";
+import { Box } from '@mui/system';
+import { IconButton, Link, Typography } from '@mui/material';
+import classes from './Footer.module.css';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
+import Grid from '@mui/material/Grid';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className={classes["grid-container"]}>
-        <div className={classes.grid__header}>
-          <h3 className={classes.h3}>Em Stuart Injuries Clinic</h3>
+	return (
+		<footer>
+			<Grid
+				container
+				spacing={2}
+				sx={{ background: '#303030' }}
+			>
+				<Grid
+					item
+					xs={12}
+				>
+					<Box>
+						<Typography
+							sx={{ color: '#ffffff', textTransform: 'uppercase' }}
+							variant="h3"
+						>
+							Em Stuart Injuries Clinic
+						</Typography>
+					</Box>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+				>
+					<Box sx={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+						<a
+							href="https://www.facebook.com/Emstuartinjuries/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<IconButton
+								color="secondary"
+								aria-label="Facebook"
+								size="large"
+								sx={{
+									border: '2px solid #25aaf7',
+									'&:hover': {
+										borderColor: 'white',
+										'& .MuiSvgIcon-root': {
+											color: 'white',
+										},
+									},
+									transition: 'border-color 0.3s, color 0.3s',
+								}}
+							>
+								<FacebookIcon />
+							</IconButton>
+						</a>
+						<a
+							href="https://api.whatsapp.com/send?phone=447964991445"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<IconButton
+								color="secondary"
+								aria-label="WhatsApp"
+								size="large"
+								sx={{
+									border: '2px solid #25aaf7',
+									'&:hover': {
+										borderColor: 'white',
+										'& .MuiSvgIcon-root': {
+											color: 'white',
+										},
+									},
+									transition: 'border-color 0.3s, color 0.3s',
+								}}
+							>
+								<WhatsAppIcon />
+							</IconButton>
+						</a>
+						<a
+							href="https://www.google.com/search?q=em+stuart+injuries#ip=1"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<IconButton
+								color="secondary"
+								aria-label="Google"
+								size="large"
+								sx={{
+									border: '2px solid #25aaf7',
+									'&:hover': {
+										borderColor: 'white',
+										'& .MuiSvgIcon-root': {
+											color: 'white',
+										},
+									},
+									transition: 'border-color 0.3s, color 0.3s',
+								}}
+							>
+								<GoogleIcon />
+							</IconButton>
+						</a>
+					</Box>
+				</Grid>
+				<Grid
+					item
+					md={4}
+					xs={12}
+				>
+					<Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '10px' }}>
+						<Typography sx={{ color: '#fff' }}>Contact Us</Typography>
+						<Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+							<Typography sx={{ color: '#fff' }}>Office 9, Unit 2</Typography>
+							<Typography sx={{ color: '#fff' }}>Morelands Trading Estate</Typography>
+							<Typography sx={{ color: '#fff' }}>Bristol Road</Typography>
+							<Typography sx={{ color: '#fff' }}>Gloucester</Typography>
+							<Typography sx={{ color: '#fff' }}>GL1 5RZ</Typography>
+						</Box>
+					</Box>
+				</Grid>
+				<Grid
+					item
+					md={4}
+					xs={12}
+				>
+					<Box>
+						<iframe
+							title="map"
+							width="100%"
+							height="250px"
+							id="gmap_canvas"
+							src="https://maps.google.com/maps?q=Em%20Stuart%20Injuries&t=&z=13&ie=UTF8&iwloc=&output=embed"
+							frameBorder="0"
+							scrolling="no"
+						></iframe>
+					</Box>
+				</Grid>
+				<Grid
+					item
+					md={4}
+					xs={12}
+				>
+					<Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '10px' }}>
+						<Typography sx={{ color: '#fff' }}>Opening Hours</Typography>
+						<Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+							<Typography sx={{ color: '#fff' }}>
+								<b>Monday:</b> Closed
+							</Typography>
+							<Typography sx={{ color: '#fff' }}>
+								<b>Tuesday:</b> 07:30am - 15:30pm
+							</Typography>
+							<Typography sx={{ color: '#fff' }}>
+								<b>Wednesday:</b> 11:30am - 19:30pm
+							</Typography>
+							<Typography sx={{ color: '#fff' }}>
+								<b>Thursday:</b> 10:00am - 16:30pm
+							</Typography>
+							<Typography sx={{ color: '#fff' }}>
+								<b>Friday:</b> 10:30 - 16:30pm
+							</Typography>
+							<Typography sx={{ color: '#fff' }}>
+								<b>Saturday:</b> 08:30 - 15:30pm
+							</Typography>
+							<Typography sx={{ color: '#fff' }}>
+								<b>Sunday:</b> Closed
+							</Typography>
+						</Box>
+					</Box>
+				</Grid>
 
-          <a
-            href="https://www.facebook.com/Emstuartinjuries/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className={classes.button}>
-              <i
-                className={`${classes.icon} ${classes["icon--primary"]} fa-brands fa-facebook-f`}
-              ></i>
-            </button>
-          </a>
-
-          <a
-            href="https://api.whatsapp.com/send?phone=447964991445"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className={classes.button}>
-              <i
-                className={`${classes.icon} ${classes["icon--primary"]} fa-brands fa-whatsapp`}
-              ></i>
-            </button>
-          </a>
-          <a
-            href="https://www.google.com/search?q=em+stuart+injuries"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className={classes.button}>
-              <i
-                className={`${classes.icon} ${classes["icon--primary"]} fa-brands fa-google`}
-              ></i>
-            </button>
-          </a>
-        </div>
-        <div className="section1">
-          <span className={classes.grid__heading}>Contact Us</span>
-          <div className={classes.grid__content}>
-            Office 9, Unit 2 <br />
-            Morelands Trading Estate
-            <br />
-            Bristol Road
-            <br />
-            Gloucester <br />
-            GL1 5RZ
-          </div>
-        </div>
-        <div className="section2">
-          <span className={classes.grid__heading}>Find Us</span>
-          <div className="mapouter">
-            <div className={classes.grid__content}>
-              <iframe
-                title="map"
-                width="95%"
-                height="80%"
-                id="gmap_canvas"
-                src="https://maps.google.com/maps?q=Em%20Stuart%20Injuries&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                frameBorder="0"
-                scrolling="no"
-              ></iframe>
-            </div>
-          </div>
-        </div>
-
-        <div className="section3">
-          <span className={classes.grid__heading}>Clinic Hours</span>
-          <div className={classes.grid__content}>
-            <b>Monday - Friday:</b> 08:00am - 08:00pm
-            <br />
-            <b>Saturday:</b> 08:00am - 12:00pm
-            <br />
-            <b>Sunday:</b> Closed
-          </div>
-        </div>
-        <div className={classes.grid__copyright}>
-          © 2022 Copyright
-          <br />
-          Privacy Policy | Terms & Conditions | Cookie Policy
-        </div>
-      </div>
-    </footer>
-  );
+				<Grid
+					item
+					xs={12}
+				>
+					<Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '10px' }}>
+						<Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+							<Typography sx={{ color: '#fff' }}>© 2022 Copyright</Typography>
+							<Box
+								sx={{ display: 'flex', gap: '10px', alignItems: 'center', paddingBottom: '1rem' }}
+							>
+								<Link
+									href="/"
+									color="secondary"
+								>
+									Privacy Policy
+								</Link>
+								<Typography sx={{ color: '#fff' }}>|</Typography>
+								<Link
+									href="/"
+									color="secondary"
+								>
+									Terms & Conditions
+								</Link>
+								<Typography sx={{ color: '#fff' }}>|</Typography>
+								<Link
+									href="/"
+									color="secondary"
+								>
+									Cookie Policy
+								</Link>
+							</Box>
+						</Box>
+					</Box>
+				</Grid>
+			</Grid>
+		</footer>
+	);
 };
 
 export default Footer;
